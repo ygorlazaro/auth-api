1a8ab9916fb6eff0d3a1f227b6afca6f246c3485
Ygor Lazaro <ygor@ygorlazaro.com>
2022-09-15 15:46:02 -0300

    Adding cors support

--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,12 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule); 
+  app.enableCors({
+    allowedHeaders: "*",
+    methods: "*",
+    origin: "*"
+  });
   await app.listen(3000);
   console.log(`Application is running on: ${await app.getUrl()}`);
 }
